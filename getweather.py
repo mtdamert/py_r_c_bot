88b57b767890e67f83dcef22d654c30c4944f60d
Sean McLaughlan <sean.s.mclaughlan@gmail.com>
2020-09-05 16:20:24 -0700

    update bot with .choose, .covid, new .weather

--- getweather.py
+++ getweather.py
@@ -1,28 +1,48 @@
 import urllib.request
-import xml.etree.ElementTree as ET
-import pgeocode
+import json
 
-def printweather(zip_code):
-  zip_code = zip_code.strip()
+# TODO make an object mapping cities to their lat and lon so that city names can be typed in instead
+# locationDict = { "LA": { "lat": 0, "lon": 0}}
 
-  if zip_code is None or zip_code == "":
-    return "But where? Please add a zip code after the weather command"
 
-  nomi = pgeocode.Nominatim('us')
-  latitude = nomi.query_postal_code(zip_code).latitude
-  latitude = str(latitude)
-  longitude = nomi.query_postal_code(zip_code).longitude
-  longitude = str(longitude)
+def printweather(lat, lon):
+  # ...with TODO above. if lat in locationDict.keys(): locationDict[lat] etc...
+  # rename lat to latOrCity?
+    try:
+        data = urllib.request.urlopen(
+            f"https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude=hourly,daily,minutely&units=imperial&appid=4ba6a408966bcec7ac3ae7026c9ab365")
+        wData = json.loads(data.read())
+        # print(readableData) #prints very poorly
+        # below is human readable
+        # print(json.dumps(wData, indent=4, sort_keys=True))
+        return f"{wData['current']['temp']} fahrenheit. {wData['current']['humidity']}% humidity. Feels like {wData['current']['feels_like']} fahrenheit. Generally: {wData['current']['weather'][0]['description']}."
+    except:
+        return "Something went wrong."
 
-  # todo: get proper url for this user
-  site = urllib.request.urlopen("https://forecast.weather.gov/MapClick.php?lat=" + latitude + "&lon=" + longitude + "&unit=0&lg=english&FcstType=dwml")
-  site_source = site.read()
-  xml_root = ET.fromstring(site_source)
+# Old weather:
 
-  weather_location = xml_root.find("./head/source/production-center")
-  fahrenheit = xml_root.find("./data[@type='current observations']/parameters/temperature[@units='Fahrenheit']/value")
-  conditions = xml_root.find("./data[@type='current observations']/parameters/weather/weather-conditions[@weather-summary]")
-  return ("Current weather in " + weather_location.text + ": " + fahrenheit.text + "F, " + conditions.attrib['weather-summary'])
+# import urllib.request
+# import xml.etree.ElementTree as ET
+# import pgeocode
 
-#test
-#print(printweather())
+# def printweather(zip_code):
+#   zip_code = zip_code.strip()
+
+#   if zip_code is None or zip_code == "":
+#     return "But where? Please add a zip code after the weather command"
+
+#   nomi = pgeocode.Nominatim('us')
+#   latitude = nomi.query_postal_code(zip_code).latitude
+#   latitude = str(latitude)
+#   longitude = nomi.query_postal_code(zip_code).longitude
+#   longitude = str(longitude)
+
+#   # todo: get proper url for this user
+#   site = urllib.request.urlopen("https://forecast.weather.gov/MapClick.php?lat=" + latitude + "&lon=" + longitude + "&unit=0&lg=english&FcstType=dwml")
+#   site_source = site.read()
+#   xml_root = ET.fromstring(site_source)
+
+#   weather_location = xml_root.find("./head/source/production-center")
+#   fahrenheit = xml_root.find("./data[@type='current observations']/parameters/temperature[@units='Fahrenheit']/value")
+#   conditions = xml_root.find("./data[@type='current observations']/parameters/weather/weather-conditions[@weather-summary]")
+#   return ("Current weather in " + weather_location.text + ": " + fahrenheit.text + "F, " + conditions.attrib['weather-summary'])
