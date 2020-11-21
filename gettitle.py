c88878d3ae528fa4c91151ea47c2e2c69ca06ea3
Sean McLaughlan <sean.s.mclaughlan@gmail.com>
2020-11-20 16:55:57 -0800

    Update gettitle to use open graph tags if title tags are not found

--- gettitle.py
+++ gettitle.py
@@ -6,8 +6,20 @@ def getPageTitle(url):
     try:
         site = urllib.request.urlopen(url)
         siteStr = site.read().decode('utf-8')
-        title = siteStr.split('<title>')[1].split('</title>')[0]
-        title = html.unescape(title)
-        return title
+
+        usesTitleTag = True if siteStr.find('<title>') != -1 else False
+        if usesTitleTag:
+            title = siteStr.split('<title>')[1].split('</title>')[0]
+            title = html.unescape(title)
+            return title
+
+        usesOpenGraph = True if siteStr.find('og:title') else False
+        if usesOpenGraph:
+            title = siteStr.split('og:title')[1].split(
+                'content=')[1]
+            splitChar = title[0]
+            title = title.split(f"{splitChar}")[1]
+            title = html.unescape(title)
+            return title
     except:
         print(f'URL title lookup failed for {url}')
